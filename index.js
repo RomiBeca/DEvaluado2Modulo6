7e76ae9b7549690740474e5e76b21756ea61a668
Romina Beca <rominabeca.v@gmail.com>
2024-05-02 23:55:56 -0400

    avance desafio

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import express from 'express'
 import { engine } from 'express-handlebars';
 import path from 'path'
+import { services, products } from './modulo.servicios.js';
 
 const app = express()
-
-// ruta absoluta
 const __dirname = import.meta.dirname
 
 // middleware archivos estáticos
@@ -25,62 +24,9 @@ app.get('/about', (req, res) => {
     res.render('about')
 })
 
-const products = [
-    { id: 1, title: "ZTE", img: "https://www.pcfactory.cl/public/foto/50037/1_200.jpg?t=1713542184599" },
-    { id: 2, title: "Motorola", img: "https://www.pcfactory.cl/public/foto/48183/1_200.jpg?t=1712348209323" },
-    { id: 3, title: "Motorola", img: "https://www.pcfactory.cl/public/foto/44473/1_200.jpg?t=1709222667989" },
-    { id: 4, title: "Xiaomi", img: "https://www.pcfactory.cl/public/foto/50697/1_200.jpg?t=1712349941174" },
-]
-
-const fruts = ['🍉', '🍓', '🍌']
-
 app.get('/products', (req, res) => {
     res.render('products', { products: products, fruts })
 })
-
-const services = [
-    {
-        name: "Web Development",
-        description: "We create web applications with the latest technologies.",
-        url: "/services/web-development",
-    },
-    {
-        name: "Mobile Development",
-        description: "We create mobile applications with the latest technologies.",
-        url: "/services/mobile-development",
-    },
-    {
-        name: "DevOps",
-        description: "We create CI/CD pipelines with the latest technologies.",
-        url: "/services/devops",
-    },
-    {
-        name: "QA",
-        description: "We create test cases with the latest technologies.",
-        url: "/services/qa",
-    },
-    {
-        name: "UX/UI",
-        description: "We create designs with the latest technologies.",
-        url: "/services/ux-ui",
-    },
-    {
-        name: "Training",
-        description: "We create training with the latest technologies.",
-        url: "/services/training",
-    },
-    {
-        name: "Consulting",
-        description: "We create consulting with the latest technologies.",
-        url: "/services/consulting",
-    },
-    {
-        name: "Outsourcing",
-        description: "We create outsourcing with the latest technologies.",
-        url: "/services/outsourcing",
-    },
-];
-
 
 app.get(`/services`, (req, res) => {
     res.render('services', { services });
---
+++ modulo.servicios.js
@@ -0,0 +1,51 @@
+export const services = [
+    {
+        name: "Web Development",
+        description: "We create web applications with the latest technologies.",
+        url: "/services/web-development",
+    },
+    {
+        name: "Mobile Development",
+        description: "We create mobile applications with the latest technologies.",
+        url: "/services/mobile-development",
+    },
+    {
+        name: "DevOps",
+        description: "We create CI/CD pipelines with the latest technologies.",
+        url: "/services/devops",
+    },
+    {
+        name: "QA",
+        description: "We create test cases with the latest technologies.",
+        url: "/services/qa",
+    },
+    {
+        name: "UX/UI",
+        description: "We create designs with the latest technologies.",
+        url: "/services/ux-ui",
+    },
+    {
+        name: "Training",
+        description: "We create training with the latest technologies.",
+        url: "/services/training",
+    },
+    {
+        name: "Consulting",
+        description: "We create consulting with the latest technologies.",
+        url: "/services/consulting",
+    },
+    {
+        name: "Outsourcing",
+        description: "We create outsourcing with the latest technologies.",
+        url: "/services/outsourcing",
+    },
+];
+
+export const products = [
+    { id: 1, title: "ZTE", img: "https://www.pcfactory.cl/public/foto/50037/1_200.jpg?t=1713542184599" },
+    { id: 2, title: "Motorola", img: "https://www.pcfactory.cl/public/foto/48183/1_200.jpg?t=1712348209323" },
+    { id: 3, title: "Motorola", img: "https://www.pcfactory.cl/public/foto/44473/1_200.jpg?t=1709222667989" },
+    { id: 4, title: "Xiaomi", img: "https://www.pcfactory.cl/public/foto/50697/1_200.jpg?t=1712349941174" },
+]
+
+export const fruts = ['🍉', '🍓', '🍌']
